ba263646079672e5882473ecfa93e62c9a10a78b
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-07 01:56:51 +0000

    separated out services and created Kubernetes manifests

--- services/frontend/server.js
+++ services/frontend/server.js
@@ -8,21 +8,25 @@ const PORT = process.env.NODE_PORT || 8080;
 const ENV = process.env.NODE_ENV || "development";
 const express = require("express");
 const bodyParser = require("body-parser");
+const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require('morgan');
 
-// PG database client/connection setup
-const { Pool } = require('pg').native;
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -31,8 +35,8 @@ const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/users", usersRoutes());
+app.use("/api/widgets", widgetsRoutes());
 // Note: mount other resources here, using the same pattern above
 
 
@@ -44,5 +48,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend listening on port ${PORT}`);
+  console.log(`Frontend listening on port ${PORT}`);
 });
