ead75036e53bd35df663fd55dd1526388288cfad
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-07 18:13:27 +0000

    bug fixes with deployments and added local dev option with kubectl, skafold, kind & docker

--- services/backend/project.sh
+++ services/backend/project.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-while ! pg_isready -d postgresql://username:password@postgres:5432/database > /dev/null ; do sleep .01; done;
-
-#reset table using resetdb.js
-# npm run db:reset
-#psql after reset
-psql postgresql://username:password@postgres:5432/database << EOF
+while ! $(psql "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}") ; do sleep 1; done;
+psql "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" << EOF
 \i db/schema/01_users.sql;
 \i db/schema/05_widgets.sql;
 \i db/seeds/01_users.sql;
 \i db/seeds/05_widgets.sql;
 \dt
 EOF
-npm start
+exec npm start
