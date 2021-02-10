43a67fe8866b992e93e3615f88253040185ca950
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-10 20:51:47 +0000

    auto commit by Bryce

--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -1,9 +1,10 @@
 const state = (() => {
+  const startingURL = document.location;
   const actions = [];
   return (action) => {
     actions.push(action);
     console.log(actions);
-    return actions;
+    return { actions, startingURL };
   };
 })();
 
