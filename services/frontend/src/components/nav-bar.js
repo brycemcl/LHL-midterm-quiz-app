a241ddcbe14d136209bc6845e624ee7cfde885b2
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-10 18:58:16 +0000

    auto commit by Bryce

---
+++ services/frontend/src/components/nav-bar.js
@@ -0,0 +1,18 @@
+export default (() => {
+  class CustomElement extends HTMLElement {
+    constructor() {
+      super();
+      const htmlTemplate = `
+      <div class="bg--800 flex justify-between">
+      <!-- logo -->
+      <div class="">Lorem, ipsum dolor.</div>
+      <!-- nav -->
+      <div class="font-title">Lorem, ipsum dolor.</div>
+    </div>
+        `;
+      this.innerHTML = htmlTemplate;
+      // this.querySelector('.contents').innerHTML = this.getAttribute('text');
+    }
+  }
+  window.customElements.define('app-shell', CustomElement);
+});
