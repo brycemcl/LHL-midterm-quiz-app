839015aad6572d7b60cb2ba4bb57c5b129b97774
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-10 19:49:27 +0000

    auto commit by Bryce

--- services/frontend/src/components/nav-button.js
+++ services/frontend/src/components/nav-button.js
@@ -7,10 +7,10 @@ export default (() => {
           <div class="font-title self-center">Lorem, ipsum dolor.</div>
         `;
       this.innerHTML = htmlTemplate;
-      // this.addEventListener("click", () => {
-      //   console.log("here");
-      // });
-      // this.querySelector('div').innerHTML = this.getAttribute('text');
+      this.addEventListener("click", () => {
+        console.log("here");
+      });
+      this.querySelector('div').innerHTML = this.getAttribute('text');
     }
   }
   window.customElements.define('nav-button', CustomElement);
