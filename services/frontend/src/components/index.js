8328af01b98d75785cc765bc26bf03ef77e04c7a
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-10 19:49:17 +0000

    auto commit by Bryce

--- services/frontend/src/components/index.js
+++ services/frontend/src/components/index.js
@@ -1,3 +1,4 @@
 import('./app-logo.js').then(c => c.default());
 import('./app-shell.js').then(c => c.default());
 import('./nav-bar').then(c => c.default());
+import('./nav-button').then(c => c.default());
