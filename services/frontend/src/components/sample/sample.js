db0970cc9ab8f8c92c964a74823d9f67f95a81d1
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-10 18:53:54 +0000

    auto commit by Bryce

--- services/frontend/src/components/sample/sample.js
+++ services/frontend/src/components/sample/sample.js
@@ -3,15 +3,9 @@ export default (() => {
     constructor() {
       super();
       const htmlTemplate = `
-        <div class="flex-row centered px-12 py-6">
-        <nav-bar class="container centered"></nav-bar>
-        <div class="contents">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At veritatis adipisci quas possimus ipsa? Voluptates, tenetur! Quas voluptatem omnis deleniti ratione placeat, commodi iusto, laudantium unde sequi, delectus eveniet ipsa.
-        </div>
-        </div>
         `;
       this.innerHTML = htmlTemplate;
-      this.querySelector('.contents').innerHTML = this.getAttribute('text');
+      // this.querySelector('.contents').innerHTML = this.getAttribute('text');
     }
   }
   window.customElements.define('app-shell', CustomElement);
