941827cfb85d20bc019290f0c440363685d6f9ef
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-10 22:45:06 +0000

    auto commit by Bryce

--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -1,4 +1,14 @@
-import { $, jQuery } from 'jquery';
+import { $ } from 'jquery';
+
+$.ajax({
+  url: '/api/',
+  data: {
+    format: 'json'
+  }
+}).then(() => { }).catch(() => { });
+
+
+
 const state = (() => {
   const url = document.location;
   const actions = [];
