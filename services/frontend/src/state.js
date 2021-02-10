ff6e8b8a93f372d26dfd8c32af24a5527f55ec72
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-10 23:01:38 +0000

    auto commit by Bryce

--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -2,19 +2,14 @@ import jQuery from 'jquery';
 const $ = jQuery;
 const api = {};
 const state = {};
-// $.ajax({
-//   url: '/api/',
-//   data: {
-//     format: 'json'
-//   }
-// }).then((r) => { console.log(r); }).catch((e) => { console.log(e); });
-api.getRecentQuizes = () => {
-  $.getJSON('/api/', (r) => {
-    state.recentQuizes = r;
-  });
+
+const updateRecentQuizes = () => {
+  api.getRecentQuizes = () => {
+    $.getJSON('/api/', (r) => {
+      state.recentQuizes = r;
+    });
+  };
 };
-
-
 
 const updateState = (() => {
   const url = document.location;
