daf348db0b52f1b252fb5aefe00b5bab10ee8478
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-11 03:26:47 +0000

    auto commit by Bryce

--- services/frontend/src/components/nav-bar.js
+++ services/frontend/src/components/nav-bar.js
@@ -22,7 +22,7 @@ export default (() => {
         $(navButtons).empty();
         c.forEach(element => {
           const button = document.createElement("template");
-          console.log(element);
+          console.log(element.name);
           button.innerHTML = `<nav-button text="${element.name}" page="${element.url}"></nav-button>`;
           navButtons.appendChild(button.content.cloneNode(true));
         });
