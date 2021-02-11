fa57c947f39a51ebf300255f49b0738debd1b8a4
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-11 02:33:12 +0000

    auto commit by Bryce

--- services/frontend/src/components/nav-bar.js
+++ services/frontend/src/components/nav-bar.js
@@ -20,7 +20,7 @@ export default (() => {
       import('./../state').then(c => c.default({ actionType: "getNavPages", data: {} })).then((c) => {
         c.forEach(element => {
           console.log(element);
-          const button = new document.createElement("nav-button");
+          const button = document.createElement("nav-button");
           button.outerHTML = `<nav-button text="${element}"></nav-button>`;
           console.log(navBar);
         });
