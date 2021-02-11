178ec2f732377758a3fba1362c7ebce295cc11d8
Zoey Farago <zf.zoodle@gmail.com>
2021-02-11 01:58:41 +0000

    api routes connecting backend to frontend

---
+++ services/frontend/src/makerStateFunction.js
@@ -0,0 +1,88 @@
+api.getQuizzesByIdCreated = () => {
+  $.getJSON(`/api/quiz-maker/user/${id}`, (response) => {
+    state.quizzesCreated = response;
+    updateDom.updateQuizzesByIdCreated();
+    return response;
+  });
+};
+
+api.getQuizById = () => {
+  $.getJSON(`/api/quiz-maker/${id}`, (response) => {
+    state.specificQuiz = response;
+    updateDom.updateQuizById();
+    return response;
+  });
+};
+
+updateDom.updateQuizById = () => {
+  const container = qs("");
+  if (state.page === "home") {
+
+  } else {
+
+  }
+};
+
+api.editQuiz = () => {
+  $.ajax({
+    url: '/api/quiz-maker/quiz',
+    method: 'POST',
+    data: $(this).serialize()
+  })
+  .then(() => {
+    console.log('Success!');
+  })
+};
+
+api.editQuestion = () => {
+  $.ajax({
+    url: '/api/quiz-maker/question',
+    method: 'POST',
+    data: $(this).serialize()
+  })
+  .then(() => {
+    console.log('Question edited succesfully!');
+  })
+};
+
+api.editOption = () => {
+  $.ajax({
+    url: '/api/quiz-maker/option',
+    method: 'POST',
+    data: $(this).serialize()
+  })
+  .then(() => {
+
+  })
+};
+
+api.addQuiz = () => {
+  $.ajax({
+    url: '/api/quiz-maker/',
+    method: 'POST',
+    data: $(this).serialize()
+  .then(() => {
+    console.log('quiz added!')
+  })
+}
+
+api.deleteQuiz = () => {
+  $.ajax({
+    url: `/api/quiz-maker/${id}/delete`,
+    method: 'POST',
+    data: /*onclick submitted data*/})
+  .then(() => {
+
+  })
+}
+
+module.exports = {
+  getQuizzesByIdCreated,
+  getQuizById,
+  updateQuizById,
+  editQuiz,
+  editQuestion,
+  editOption,
+  addQuiz,
+  deleteQuiz
+}
--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -7,11 +7,15 @@ const state = {
   page: "home"//update based off of url of page
 };
 
+const _ = require('./makerStateFunction');
+
+
 // setInterval(() => {
 //   console.clear();
 //   console.log(state);
 // }, 5000);
 
+// get all the quizzes
 api.getRecentQuizzes = () => {
   $.getJSON('/api/', (response) => {
     state.recentQuizzes = response;
@@ -19,7 +23,29 @@ api.getRecentQuizzes = () => {
     return response;
   });
 };
+
 updateDom.updateRecentQuizzes = () => {
+  const container = qs("");
+  //update dom here
+};
+
+api.getQuizzesTakenByUser = () => {
+  $.getJSON`'/api/quiz-taker/user/${id}`, (response) => {
+    state.recentQuizzes = response;
+    updateDom.updateQuizzesTaken();
+    return response;
+  }
+};
+
+api.getQuizById = () => {
+  $.getJSON(`/api/quiz-taker/${id}`, (response) => {
+    state.specificQuiz = response;
+    updateDom.updateQuizById();
+    return response;
+  });
+};
+
+updateDom.updateQuizzesTaken = () => {
   const container = qs("");
   if (state.page === "home") {
 
@@ -29,15 +55,22 @@ updateDom.updateRecentQuizzes = () => {
   //update dom here
 };
 
+$.ajax({ url: `/api/quiz-taker/${id}/answer`, method: 'POST', data: /*onclick submitted data*/})
+.then(() => {
 
+})
 
+$.ajax({ url: `/api/quiz-taker/${id}/score`, method: 'POST', data: /*onclick submitted data*/})
+.then(() => {
+
+})
+
+$.ajax({ url: `/api/quiz-taker/${id}/delete`, method: 'POST', data: /*onclick submitted data*/})
+.then(() => {
+
+})
 
 // api.getRecentQuizzes();
-
-
-
-
-
 const updateState = (() => {
   // const actions = [];
   return (action) => {
