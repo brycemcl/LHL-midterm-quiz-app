71df11846f49d2f81a7a3603941a58a70e3588c0
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-11 03:32:08 +0000

    auto commit by Bryce

--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -21,7 +21,7 @@ events.getNavPages = () => {
   }
   return pagesToReturn.map((item) => pages[item]);
 };
-events.changePage = (data) => {
+events.changePage = ({ data }) => {
   console.log(data);
   // if (state.page) {
   //   return [{ name: "Home", url: "/" }, { name: "My Quizzes", url: "/my-quizzes" }, "Browse Quizzes", "Log Out"];
