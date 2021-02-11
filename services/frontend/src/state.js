db730821f8478e1959e96bd4e4c4ca3ccb0b8178
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-11 02:03:33 +0000

    auto commit by Bryce

--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -1,12 +1,11 @@
 import jQuery from 'jquery';
 const $ = jQuery;
 const qs = Document.querySelector;
-const api = {};
-const updateDom = {};
+const events = {};
 const state = {};
 state.page = "home"; //update based off of url of page
 
-api.getNavPages = () => {
+events.getNavPages = () => {
   if (state.page === "home") {
     return ["Home", "All Quizzes"];
   } else if (state.page === "take") {
@@ -23,71 +22,50 @@ api.getNavPages = () => {
 const _ = require('./makerStateFunction');
 
 
-// setInterval(() => {
-//   console.clear();
-//   console.log(state);
-// }, 5000);
 
 // get all the quizzes
-api.getRecentQuizzes = () => {
+events.getRecentQuizzes = () => {
   $.getJSON('/api/', (response) => {
     state.recentQuizzes = response;
-    updateDom.updateRecentQuizzes();
     return response;
   });
 };
 
-updateDom.updateRecentQuizzes = () => {
-  const container = qs("");
-  //update dom here
-};
-
-api.getQuizzesTakenByUser = () => {
-  $.getJSON`'/api/quiz-taker/user/${id}`, (response) => {
+events.getQuizzesTakenByUser = () => {
+  $.getJSON(`'/api/quiz-taker/user/${id}`, (response) => {
     state.recentQuizzes = response;
-    updateDom.updateQuizzesTaken();
     return response;
-  }
+  });
 };
 
-api.getQuizById = () => {
+events.getQuizById = () => {
   $.getJSON(`/api/quiz-taker/${id}`, (response) => {
     state.specificQuiz = response;
-    updateDom.updateQuizById();
     return response;
   });
 };
 
-updateDom.updateQuizzesTaken = () => {
-  const container = qs("");
-  if (state.page === "home") {
-
-  } else {
-
-  }
-  //update dom here
-};
 
 $.ajax({ url: `/api/quiz-taker/${id}/answer`, method: 'POST', data: /*onclick submitted data*/})
-.then(() => {
+  .then(() => {
 
-})
+  });
 
 $.ajax({ url: `/api/quiz-taker/${id}/score`, method: 'POST', data: /*onclick submitted data*/})
-.then(() => {
+  .then(() => {
 
-})
+  });
 
 $.ajax({ url: `/api/quiz-taker/${id}/delete`, method: 'POST', data: /*onclick submitted data*/})
-.then(() => {
+  .then(() => {
 
-})
+  });
 
 // api.getRecentQuizzes();
 const updateState = (() => {
   return (action) => {
     const { actionType, data = null } = action;
-    return api[actionType]({ data });
+    return events[actionType]({ data });
   };
 })();
 
