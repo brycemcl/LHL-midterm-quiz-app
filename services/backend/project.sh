0ad68c117d794b6f96e1a9ebb1ca3d7cb91c6a36
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-11 20:12:42 +0000

    changed schema to be in the right order

--- services/backend/project.sh
+++ services/backend/project.sh
@@ -6,18 +6,18 @@ psql "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POST
 psql "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"  -c "CREATE DATABASE ${POSTGRES_DB};"
 #setup db
 psql "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" << EOF
-\i db/schema/02_quizzes.sql;
 \i db/schema/01_users.sql;
+\i db/schema/02_quizzes.sql;
+\i db/schema/03_questions.sql;
 \i db/schema/04_options.sql;
 \i db/schema/05_answers.sql;
 \i db/schema/06_options_answers.sql;
-\i db/schema/03_questions.sql;
-\i db/seeds/02_quizzes.sql;
 \i db/seeds/01_users.sql;
+\i db/seeds/02_quizzes.sql;
+\i db/seeds/03_questions.sql;
 \i db/seeds/04_options.sql;
 \i db/seeds/05_answers.sql;
 \i db/seeds/06_options_answers.sql;
-\i db/seeds/03_questions.sql;
 \dt
 EOF
 exec npm start
