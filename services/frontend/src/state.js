9f4924d81eed06fb497f9b185740aa8d42923d67
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-11 01:48:24 +0000

    auto commit by Bryce

--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -54,7 +54,8 @@ updateDom.updateRecentQuizzes = () => {
 const updateState = (() => {
   return (action) => {
     const { actionType, data = null } = action;
-    return api[action.actionType]({ data })
+    console.log(api[actionType]);
+    return api[actionType]({ data })
       .then((result) => {
         return { state };
       }).catch((err) => {
