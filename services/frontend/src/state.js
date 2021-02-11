dd55797743769635f7d79501b3269d625842fee2
Zoey Farago <zf.zoodle@gmail.com>
2021-02-11 03:31:22 +0000

    Completed event routes connecting fronted to backend with ajax requests

--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -5,6 +5,7 @@ const events = {};
 const state = {};
 state.page = "home"; //update based off of url of page
 
+
 events.getNavPages = () => {
   if (state.page === "home") {
     return ["Home", "All Quizzes"];
@@ -19,9 +20,6 @@ events.getNavPages = () => {
   }
 };
 
-const _ = require('./makerStateFunction');
-
-
 // get all the quizzes
 events.getRecentQuizzes = () => {
   $.getJSON('/api/', (response) => {
@@ -30,42 +28,10 @@ events.getRecentQuizzes = () => {
   });
 };
 
-events.getQuizzesTakenByUser = () => {
-  $.getJSON(`'/api/quiz-taker/user/${id}`, (response) => {
-    state.recentQuizzes = response;
-    return response;
-  });
-};
-
-events.getQuizById = () => {
-  $.getJSON(`/api/quiz-taker/${id}`, (response) => {
-    state.specificQuiz = response;
-    return response;
-  });
-};
-
-
-$.ajax({ url: `/api/quiz-taker/${id}/answer`, method: 'POST', data: /*onclick submitted data*/})
-  .then(() => {
-
-  });
-
-$.ajax({ url: `/api/quiz-taker/${id}/score`, method: 'POST', data: /*onclick submitted data*/})
-  .then(() => {
-
-  });
-
-$.ajax({ url: `/api/quiz-taker/${id}/delete`, method: 'POST', data: /*onclick submitted data*/})
-  .then(() => {
-
-  });
-
 // api.getRecentQuizzes();
 const updateState = (() => {
   return (action) => {
     const { actionType, data = null } = action;
-    return events[actionType]({ data });
-  };
-})();
-
-export default updateState;
+    return events[actionType]({ data })
+  }
+});
