11bd03d48333f668893711362bfe528a7208e98c
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-12 05:30:28 +0000

    9:30pm progress

--- services/frontend/src/makerStateFunction.js
+++ services/frontend/src/makerStateFunction.js
@@ -1,71 +1,66 @@
 import state from './state';
 import updateState from './state';
 
-events.getQuizzesByIdCreated = (user_id) => {
-  $.getJSON(`/api/quiz-maker/user/${user_id}`, (response) => {
-    state.quizzesCreated = response;
-    return response;
-  });
-};
 
-events.getQuizById = (quiz_id) => {
+
+maker.getQuizById = (quiz_id) => {
   $.getJSON(`/api/quiz-maker/${quiz_id}`, (response) => {
     state.specificQuiz = response;
     return response;
   });
 };
 
-events.editQuiz = (quiz) => {
+maker.editQuiz = (quiz) => {
   $.ajax({
     url: '/api/quiz-maker/quiz',
     method: 'POST',
     data: quiz
   })
-  .then(() => {
-    console.log('Success!');
-  })
+    .then(() => {
+      console.log('Success!');
+    });
 };
 
-events.editQuestion = (question) => {
+maker.editQuestion = (question) => {
   const data = { question };
   $.ajax({
     url: '/api/quiz-maker/question',
     method: 'POST',
     data: question
   })
-  .then(() => {
-    console.log('question edited succesfully!');
-  })
+    .then(() => {
+      console.log('question edited succesfully!');
+    });
 };
 
-events.editOption = (option) => {
+maker.editOption = (option) => {
   $.ajax({
     url: '/api/quiz-maker/option',
     method: 'POST',
     data: option
   })
-  .then(() => {
-    console.log('option editted');
-  })
+    .then(() => {
+      console.log('option editted');
+    });
 };
 
-events.addQuiz = (quiz) => {
+maker.addQuiz = (quiz) => {
   $.ajax({
     url: '/api/quiz-maker/',
     method: 'POST',
     data: quiz
   })
-  .then(() => {
-    console.log('quiz added!');
-  })
+    .then(() => {
+      console.log('quiz added!');
+    });
 };
 
-events.deleteQuiz = (quiz_id) => {
+maker.deleteQuiz = (quiz_id) => {
   $.ajax({
     url: `/api/quiz-maker/${quiz_id}/delete`,
     method: 'POST'
   })
-  .then(() => {
-    console.log('quiz deleted');
-  })
+    .then(() => {
+      console.log('quiz deleted');
+    });
 };
