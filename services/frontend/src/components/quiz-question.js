514eac5c0013ac2d17c4a80934aba588ae24ee24
dantan123 <dtan9988@gmail.com>
2021-02-12 02:56:18 +0000

    push changes

--- services/frontend/src/components/quiz-question.js
+++ services/frontend/src/components/quiz-question.js
@@ -14,15 +14,21 @@ export default (() => {
     }
     connectedCallback() {
       const options = document.querySelector(".options");
-      const question_id = this.getAttribute('question-id');
-      import('./../state').then(c => c.default({ actionType: "getOptions", data: { question_id } })).then((o) => {
+      // const question_id = this.getAttribute('question-id');
+      const question_id = 5;
+      import('./../state')
+      .then(c => c.default({ actionType: "getOptions", data: { question_id } }))
+      .then((o) => {
         $(options).empty();
-        o.forEach(element => {
-          const option = document.createElement("template");
-          option.innerHTML = `<div class="border-solid border-0 rounded-3xl primary-card-color">${element}</div>`;
-          options.appendChild(option.content.cloneNode(true));
-        });
-      });
+        if (o) {
+          o.forEach(element => {
+            const option = document.createElement("template");
+            option.innerHTML = `<div class="border-solid border-0 rounded-3xl primary-card-color">${element}</div>`;
+            options.appendChild(option.content.cloneNode(true));
+          });
+        }
+      })
+      .catch(err => console.log('coming from quiz-question', err));
     }
   }
   window.customElements.define('quiz-question', CustomElement);
--- services/frontend/src/state.js
+++ services/frontend/src/state.js
@@ -12,11 +12,13 @@ const pages = {
   logout: { name: "Logout", page: "/" },
   login: { name: "Login", page: "/" }
 };
+
 const updateUrl = () => {
   state.page = Object.keys(pages).filter((item) => {
     return pages[item].page === window.location.pathname.split("/")[1] ? window.location.pathname.split("/")[1] : "/";
   })[0];
 };
+
 updateUrl();
 events.getNavPages = () => {
   let pagesToReturn = [];
@@ -53,6 +55,7 @@ events.getRecentQuizzes = () => {
 // setting the states of quiz_id, quiz_title, and is_current
 events.getQuiz = (quiz_id) => {
   $.getJSON(`/api/quiz-maker/${quiz_id}`, (response) => {
+    state.quiz = {};
     state.quiz_id = response[0].id;
     state.quiz_title = response[0].title;
     state.quiz_current = response[0].is_current;
@@ -62,7 +65,7 @@ events.getQuiz = (quiz_id) => {
   });
 };
 
-events.getQuiz(5);
+// events.getQuiz(5);
 
 events.getQuestionsByQuiz = (quiz_id) => {
   $.getJSON(`/api/quiz-taker/questions/${quiz_id}`, response => {
@@ -72,12 +75,11 @@ events.getQuestionsByQuiz = (quiz_id) => {
     if (state.quiz_id) {
       for (const question of response) {
         console.log("question is", question);
-        state.question_id = {};
+        // state.question_id.quiz_id = quiz_id;
         state.question_id.question = question.question;
         state.question_id.question_pic_url = question.question_pic_url;
         state.question_id.sub_text = question.sub_text;
       }
-      console.log('res:', state[quiz_id][response.id]);
     }
     // return state[quiz_id][response.id];
   });
@@ -100,9 +102,16 @@ events.getQuestion = (question_id) => {
 
 // get all quiz options for a question
 events.getOptions = (question_id) => {
+  console.log('question_id:', question_id);
   $.getJSON(`/api/quiz-taker/options/${question_id}`, response => {
-    state.question_id = response;
-    return state.question_id;
+    for (const option of response) {
+      console.log('option:', option);
+      if (state.quiz_id) {
+        console.log('state quiz id', state.quiz_id);
+        state.option_id = {}
+        state.option_id.question_id = option.question_id;
+      }
+    }
   });
 };
 
@@ -112,7 +121,9 @@ const updateState = (() => {
     return events[actionType]({ data });
   };
 })();
+
 setInterval(() => {
   console.info(state);
 }, 5000);
+
 export default updateState;
